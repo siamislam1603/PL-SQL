c52e6b7a9395dce974981231b4cea545c4702c6e
Your Name <you@example.com>
2021-07-29 17:10:19 +0600

    A project using PLSQL to find the root node of the Decision Tree

---
+++ Decision Tree/SERVER1/conn.sql
@@ -0,0 +1,15 @@
+drop database link site1;
+
+create database link site1
+ connect to system identified by "12345"
+ using '(DESCRIPTION =
+       (ADDRESS_LIST =
+         (ADDRESS = (PROTOCOL = TCP)
+		 (HOST = 192.168.227.128)
+		 (PORT = 1622))
+       )
+       (CONNECT_DATA =
+         (SID = XE)
+       )
+     )'
+;  
